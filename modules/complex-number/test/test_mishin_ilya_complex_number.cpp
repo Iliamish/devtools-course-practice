bafb964f6a3a57c17368a8a55b16890091738094
Iliamish <qazcdeedczaq123>
2021-03-22 00:34:42 +0300

    new tests

--- modules/complex-number/test/test_mishin_ilya_complex_number.cpp
+++ modules/complex-number/test/test_mishin_ilya_complex_number.cpp
@@ -2,33 +2,35 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>
+
 #include "include/complex_number.h"
 
-TEST(Mishin_Ilya_ComplexNumberTest, equalsZeroSubtraction) {
-    ComplexNumber compx1;
-    ComplexNumber compx2(1.25, 1.25);
-    ComplexNumber compx3(2.27, 2.26);
-    ComplexNumber compx4(-1.02, -1.01);
+TEST(Mishin_Ilya_ComplexNumberTest, testSequentialOperationsSub) {
+    ComplexNumber compx1(1, 1);
+    ComplexNumber compx2(1, 1);
+    ComplexNumber compx3(2, 2);
+    ComplexNumber compx4(4, 4);
 
-    compx1 = compx2 - compx3 - compx4;
-
-    ASSERT_EQ(compx1, compx2 - compx3 - compx4);
+    ASSERT_EQ(compx1, compx4 - compx2 - compx3);
 }
 
-TEST(Mishin_Ilya_ComplexNumberTest, equalsZeroMultiplication) {
-    ComplexNumber compx1;
-    ComplexNumber compx2(1.25, 1.25);
-    ComplexNumber compx3(2.23, 2.24);
-    ComplexNumber compx4;
+TEST(Mishin_Ilya_ComplexNumberTest, testSequentialOperationsMult) {
+    ComplexNumber compx1(-12,12);
+    ComplexNumber compx2(1, 1);
+    ComplexNumber compx3(2, 2);
+    ComplexNumber compx4(3, 3);
 
     ASSERT_EQ(compx1, compx2 * compx3 * compx4);
 }
 
-TEST(Mishin_Ilya_ComplexNumberTest, equalsZeroDivision) {
-    ComplexNumber compx1;
-    ComplexNumber compx2;
-    ComplexNumber compx3(1.25, 1.25);
-    ComplexNumber compx4(1.23, 1.24);
+TEST(Mishin_Ilya_ComplexNumberTest, testSequentialOperationsDiv) {
+    double re = 1.0/3.0;
+    double im = -1.0/3.0;
+    ComplexNumber compx1(re, im);
+    ComplexNumber compx2(2,2);
+    ComplexNumber compx3(3, 3);
+    ComplexNumber compx4(4, 4);
 
-    ASSERT_TRUE(compx1 / compx3 == compx2 / compx4);
+    ASSERT_EQ(compx1, compx4 / compx3 / compx2);
 }
