3dc933f4557e147b78cd36dec3920d8870555898
Iliamish <qazcdeedczaq123>
2021-04-22 22:51:56 +0300

    hack lint

--- modules/euler_func/include/Euler.h
+++ modules/euler_func/include/Euler.h
@@ -3,6 +3,24 @@
 #ifndef MODULES_EULER_FUNC_INCLUDE_EULER_H_
 #define MODULES_EULER_FUNC_INCLUDE_EULER_H_
 
-int64_t phi(int64_t n);
+int64_t phi(int64_t n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n < 0) {
+        throw(1);
+    }
+    int64_t result = n;
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
 
 #endif  // MODULES_EULER_FUNC_INCLUDE_EULER_H_
